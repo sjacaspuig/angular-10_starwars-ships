e27e096a4caf928b739978e82b233640102ceecd
Sergi Jacas <sjacaspuig@gmail.com>
2020-10-02 21:31:57 +0200

    Refactor starships-list
    Add event and property bindings

--- src/app/starships-list/starships-list.component.ts
+++ src/app/starships-list/starships-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-starships-list',
+  selector: 'starships-list',
   templateUrl: './starships-list.component.html',
   styleUrls: ['./starships-list.component.scss']
 })
 export class StarshipsListComponent implements OnInit {
 
+  @Input() starships: [] = [];
+  @Output() onFetchNextPage;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // fetchNextPage = function () {
+  //   this.onFetchNextPage();
+  // }
 
 }
