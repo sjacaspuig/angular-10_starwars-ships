43b9734c3e81db6b7001908c0bf43f98a10ef19b
Sergi Jacas <sjacaspuig@gmail.com>
2020-10-02 19:41:09 +0200

    Refactor ships service

--- src/app/services/ships.service.ts
+++ src/app/services/ships.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShipsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  GetStarships(url): Observable<any> {      
+    if (!url) {
+        url  ='https://swapi.co/api/starships/'
+    }
+
+    return this.http.get(url, {
+        headers: {
+            'Authorization': 'none'        
+        }
+    });      
+  }
 }
