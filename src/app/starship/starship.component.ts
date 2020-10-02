82ae803fec11a23173c5ec65cea6240734593238
Sergi Jacas <sjacaspuig@gmail.com>
2020-10-02 21:44:42 +0200

    Refactor starship component

--- src/app/starship/starship.component.ts
+++ src/app/starship/starship.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-starship',
+  selector: 'starship',
   templateUrl: './starship.component.html',
   styleUrls: ['./starship.component.scss']
 })
 export class StarshipComponent implements OnInit {
 
+  @Input() starship: {} = {}
+  starshipUrl = "";
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getStarshipId();
+  }
+
+  private getStarshipId(): void {
+    const url = this.starship['url'];
+    const shipId = url.split("/").filter(item => item !== "").slice(-1)[0];
+    this.starshipUrl = 'https://starwars-visualguide.com/assets/img/starships/' + shipId + '.jpg';
   }
 
 }
