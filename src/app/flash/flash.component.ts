a3325b0c8eada533d5b4a972beeb6ff8105612d3
Sergi Jacas <sjacaspuig@gmail.com>
2020-10-02 19:23:18 +0200

    Create new flash view

---
+++ src/app/flash/flash.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Flash, FlashService } from '../services/flash.service';
+
+
+@Component({
+  selector: 'flash',
+  templateUrl: './flash.component.html',
+  styleUrls: ['./flash.component.scss']
+})
+export class FlashComponent implements OnInit, OnDestroy {
+
+  @Output() update = new EventEmitter<void>();
+  flash: Flash;
+  updateSubscription: Subscription = null;
+
+  constructor(private flashService: FlashService) { }
+  
+  ngOnInit(): void {
+    this.updateSubscription = this.flashService.update$.subscribe((flash: Flash) => {
+      this.flash = flash;
+    });
+  }
+
+  ngOnDestroy() {
+    this.updateSubscription.unsubscribe();
+  }
+
+}
