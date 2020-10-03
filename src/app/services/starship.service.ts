14a05c376a57d6a060bbcf2805f65997169b886b
Sergi Jacas <sjacaspuig@gmail.com>
2020-10-03 12:09:26 +0200

    Create new starship service to call to api and get the details of current straship

---
+++ src/app/services/starship.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StarshipService {
+
+  constructor(private http: HttpClient) { }
+
+  public getStarship(id): Observable<any> {      
+    const url  ='https://swapi.dev/api/starships/' + id + '/';
+    return this.http.get(url, {headers: {'Authorization': 'none'}});      
+  }
+}
