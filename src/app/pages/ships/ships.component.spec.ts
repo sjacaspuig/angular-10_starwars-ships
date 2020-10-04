57b3b91db2e56001076f4141ab65f07a56f96c87
Sergi Jacas <sjacaspuig@gmail.com>
2020-10-04 21:14:37 +0200

    Test in ships component to getAllStarshipsByUrl call of starshipsService
    - TestBed.get is deprecated but function
    - I try with mockService but it's not the same and I can't do a real environment with http request
    - I try too with new Service() but there ara a lot of nests and finally not function

--- src/app/pages/ships/ships.component.spec.ts
+++ src/app/pages/ships/ships.component.spec.ts
@@ -1,24 +1,41 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { StarshipsService } from 'src/app/shared/services/starships.service';
 import { ShipsComponent } from './ships.component';
 
 describe('ShipsComponent', () => {
   let component: ShipsComponent;
   let fixture: ComponentFixture<ShipsComponent>;
+  let starshipsService: StarshipsService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ HttpClientModule ],
-      declarations: [ ShipsComponent ]
+      declarations: [ ShipsComponent ],
+      providers: [ StarshipsService ]
     })
     .compileComponents();
-  });
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(ShipsComponent);
     component = fixture.componentInstance;
+    starshipsService = TestBed.get(StarshipsService);
     fixture.detectChanges();
+  });
+
+
+  it('should get the data of starshipsService without url', (done) => {
+      starshipsService.getAllStarshipsByUrl(null).subscribe(() => {
+        expect(component.nextPage).toEqual('http://swapi.dev/api/starships/?page=2');
+        expect(component.nextPage).toBeNull();
+        done();
+      })
+  });
+
+  it('should get the data of starshipsService with url', (done) => {
+    starshipsService.getAllStarshipsByUrl('https://swapi.dev/api/starships/?page=2').subscribe(() => {
+      expect(component.nextPage).toEqual('http://swapi.dev/api/starships/?page=3');
+      expect(component.nextPage).toEqual('http://swapi.dev/api/starships/?page=1');
+      done();
+    })
   });
 
   it('should create', () => {
