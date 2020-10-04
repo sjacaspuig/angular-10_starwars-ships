5192eadad51d7d42d66af39d45a57375e7d7416b
Sergi Jacas <sjacaspuig@gmail.com>
2020-10-04 23:09:23 +0200

    Task 8 Done

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { FlashModule } from './components/flash/flash.module';
-import { MenuModule } from './components/menu/menu.module';
-import { PaginatorModule } from './components/paginator/paginator.module';
-import { StarshipModule } from './components/starship/starship.module';
-import { StarshipsListModule } from './components/starships-list/starships-list.module';
 
 describe('AppComponent', () => {
 
@@ -15,12 +10,7 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
-        FlashModule,
-        MenuModule,
-        PaginatorModule,
-        StarshipModule,
-        StarshipsListModule,
+        RouterTestingModule
       ],
       declarations: [
         AppComponent
