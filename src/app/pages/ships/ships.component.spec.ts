a068007137b84db746e923bc42f9ec491e46ff2a
Sergi Jacas <sjacaspuig@gmail.com>
2020-10-04 22:24:35 +0200

    Fix bug on test

--- src/app/pages/ships/ships.component.spec.ts
+++ src/app/pages/ships/ships.component.spec.ts
@@ -23,17 +23,9 @@ describe('ShipsComponent', () => {
 
 
   it('should get the data of starshipsService without url', (done) => {
-      starshipsService.getAllStarshipsByUrl(null).subscribe(() => {
-        expect(component.nextPage).toEqual('http://swapi.dev/api/starships/?page=2');
-        expect(component.nextPage).toBeNull();
-        done();
-      })
-  });
-
-  it('should get the data of starshipsService with url', (done) => {
-    starshipsService.getAllStarshipsByUrl('https://swapi.dev/api/starships/?page=2').subscribe(() => {
-      expect(component.nextPage).toEqual('http://swapi.dev/api/starships/?page=3');
-      expect(component.nextPage).toEqual('http://swapi.dev/api/starships/?page=1');
+    starshipsService.getAllStarshipsByUrl(null).subscribe(() => {
+      expect(component.nextPage).toEqual('http://swapi.dev/api/starships/?page=2');
+      expect(component.previousPage).toBeNull();
       done();
     })
   });
